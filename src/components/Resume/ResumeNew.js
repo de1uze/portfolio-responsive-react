6fa7c35391f88139ecbd6d69f5b5dfc65a8ac061
Pratik <shringarpure95@gmail.com>
2024-01-12 15:45:38 +0530

    resume updated 1

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/pratikresume1.pdf";
+import pdf from "../../Assets/pratikresume.pdf"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
